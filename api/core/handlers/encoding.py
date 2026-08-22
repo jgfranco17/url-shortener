5f56a4c37ef374161e0ce0fbb37a337ab9930e08
Chino Franco <chino.franco@gmail.com>
2026-08-22 22:07:22 +0900

    fix: shared const for encoding

--- api/core/handlers/encoding.py
+++ api/core/handlers/encoding.py
@@ -1,4 +1,7 @@
 import base64
+from typing import Final
+
+STRING_ENCODING: Final[str] = "utf-8"
 
 
 def encode_text(original_string: str) -> str:
@@ -10,9 +13,9 @@ def encode_text(original_string: str) -> str:
     Returns:
         str: ASCII-readable base64 encoded string.
     """
-    text_bytes = original_string.encode("utf-8")
+    text_bytes = original_string.encode(STRING_ENCODING)
     encoded_bytes = base64.b64encode(text_bytes)
-    encoded_string = encoded_bytes.decode("utf-8")
+    encoded_string = encoded_bytes.decode(STRING_ENCODING)
     return encoded_string
 
 
@@ -26,5 +29,5 @@ def decode_text(encoded_string: str) -> str:
         str: Decoded original text.
     """
     decoded_bytes = base64.b64decode(encoded_string)
-    original_text = decoded_bytes.decode("utf-8")
+    original_text = decoded_bytes.decode(STRING_ENCODING)
     return original_text
